e4f81392d08b8b68d9773a411e7f05f67d465afd
Harishankar999 <harishankarpatra999@gmail.com>
2022-05-17 00:23:00 +0530

    React Navbar

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Button = () =>{
+    return <div>
+    <button className="contact">Contact</button>
+  </div>
+}
+
+export {Button}
---
+++ src/components/Links.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Link = () => {
+    return <div>
+          <p>Service</p>
+          <p>Project</p>
+          <p>About</p>
+    </div>
+}
+export {Link}
